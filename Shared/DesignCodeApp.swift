c498c41b880be7f503e7fdc1c4cf77b4f13f6c1d
ingridsilapan <ingridsilapan@gmail.com>
2021-12-01 20:03:27 +1300

    Initial Commit

---
+++ Shared/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  Shared
+//
+//  Created by Ingrid Silapan on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
